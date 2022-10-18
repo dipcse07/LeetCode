386e4d893184822ede50f023dc0b0d3ae5b24004
MD SAZID HASAN DIP <dipcse07@gmail.com>
2022-10-18 11:37:26 +0900

    commited for github push

---
+++ LeetCode01.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+#include <cs50.c>
+#include <stdio.h>
+#include <string.h>
+#include<stdlib.h>
+
+
+int main(int argc, string argv[])
+{
+
+if (argc > 2) {
+
+    printf("Usage: ./caesar key");
+    return 0;
+}else {
+
+string word = get_string("plaintext:  ");
+print("%s",word);
+}
+return 0;
+}
